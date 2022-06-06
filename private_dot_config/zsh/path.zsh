c3083504105d36b4af9401cf793855d4578efa7f
stCarolas <stcarolas@gmail.com>
2022-06-06 13:25:54 +0300

    set lua LSP path without version

--- private_dot_config/zsh/path.zsh
+++ private_dot_config/zsh/path.zsh
@@ -4,4 +4,4 @@ export JAVA_HOME=/usr/lib/jvm/java-17
 export PATH=$PATH:~/.local/bin
 export PATH=$PATH:~/Coding/scripts
 export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:~/Coding/libs/lua-language-server-3.2.2-linux-x64/bin/
+export PATH=$PATH:~/Coding/libs/lua-language-server/bin/

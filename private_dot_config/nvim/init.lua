4852c4a4ae3477e48aac1f3588012354c80d9e88
stCarolas <stcarolas@gmail.com>
2024-01-10 06:35:36 +0300

    little cleanup for nvim conf

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -9,26 +9,23 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-
 vim.opt.rtp:prepend(lazypath)
+
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.termguicolors = true
-vim.cmd 'set cmdheight=0'
+vim.opt.cmdheight=0
+vim.opt.laststatus=3
+vim.opt.splitkeep="topline"
 
-vim.g.mapleader = "m"
 vim.g.neoterm_shell = "/usr/bin/zsh"
 
 vim.cmd 'set clipboard+=unnamedplus'
-vim.cmd 'set laststatus=3'
-vim.cmd 'set splitkeep=topline'
 
 vim.api.nvim_set_keymap('n',';',':', { noremap = true })
-vim.api.nvim_set_keymap('n','J','<cmd>bnext<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n','K','<cmd>bprev<cr>', { noremap = true })
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
 
 vim.g.mapleader = "m"
@@ -37,7 +34,5 @@ vim.o.foldlevelstart = 90
 
 require("lazy").setup("plugins")
 
-
 -- todo потыкать AckslD/nvim-neoclip.lua
 -- todo потыкать https://github.com/tversteeg/registers.nvim
--- todo пофиксить path и buffer автокомплиты
